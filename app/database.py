d991d5316539286add72d7916ccb35beeb50edd5
MisterKiyou <quentin.samudio@yahoo.fr>
2024-02-08 22:16:44 +0100

    Integrated Configuration File

--- app/database.py
+++ app/database.py
@@ -1,11 +1,20 @@
 import sqlite3
+import configparser
+import logging
+
 
 class Database:
-    def __init__(self, db_path):
-        self.conn = sqlite3.connect(db_path)
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+
+        # Use 'db_path' from the configuration file if provided, else use the default value
+        self.db_path = config.get('Database', 'db_path', fallback='todo.db')
+        self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
+
     def initialize_database(self):
-        try :
+        try:
             self.cursor.execute('''
                 CREATE TABLE IF NOT EXISTS tasks (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +28,7 @@ class Database:
             logging.error(f"Error initializing database: {e}")
 
     def add_task(self, task_name, due_date=None):
-        try :
+        try:
             self.cursor.execute('INSERT INTO tasks (task_name, due_date, completed) VALUES (?, ?, ?)',
                                 (task_name, due_date, False))
             self.conn.commit()
@@ -27,7 +36,7 @@ class Database:
             logging.error(f"Error adding task to database: {e}")
 
     def get_all_tasks(self):
-        try :
+        try:
             self.cursor.execute('SELECT * FROM tasks')
             return self.cursor.fetchall()
         except sqlite3.Error as e:
